bd45966517419ed3ab60fc6deaac414641b9888f
Rencist <dafianakhdan@gmail.com>
2022-10-27 19:56:05 +0700

    fix: fix typo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { OpenRecruitmentModule } from './resources/open-recruitment/open-recruitment.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { ItsModule } from './resources/its/its.module';
 import { AuthModule } from './resources/auth/auth.module';
 import { JwtModule } from './guards/jwt/jwt.module';
-import { ShortenerModule } from './resources/shortener/shortener.module';
 import { MangaModule } from './resources/manga/manga.module';
 
 const ENV = process.env.ENV;
@@ -17,12 +14,9 @@ const ENV = process.env.ENV;
     ConfigModule.forRoot({
       envFilePath: !ENV ? '.env' : `.env.${ENV}`,
     }),
-    OpenRecruitmentModule,
     PrismaModule,
-    ItsModule,
     AuthModule,
     JwtModule,
-    ShortenerModule,
     MangaModule,
   ],
   controllers: [AppController],
